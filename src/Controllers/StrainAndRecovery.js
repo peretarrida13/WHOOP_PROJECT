efb98c4d242c28ef720325b367bc20ba9c249725
Pere Tarrida Domingo <58071471+peretarrida13@users.noreply.github.com>
2024-01-29 13:32:50 +0100

    change one

--- src/Controllers/StrainAndRecovery.js
+++ src/Controllers/StrainAndRecovery.js
@@ -1,46 +1,31 @@
 
 
 export async function getStrain(accessToken) {
-    const query = new URLSearchParams({
-        limit: "1",
-    });
 
-    const uri = `https://api.prod.whoop.com/developer/v1/cycle?${query}`;
+  const uri = `https://whoop-performance-backend-e57e252e2747.herokuapp.com/api/strain/${accessToken}`;
 
-    const cycleResponse = await fetch(uri, {
-      method:'GET',
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'https://whoop-performance-frontend-a54d4e25e6a0.herokuapp.com',
+  const cycleResponse = await fetch(uri, {
+    method:'GET',
+  });
 
-      },
-    });
-  
-    if (cycleResponse.status === 200) {
-      return cycleResponse.json();
-    } else {
-      throw new Error(`Received ${cycleResponse.status} status from Whoop`);
-    }
+  if (cycleResponse.status === 200) {
+    return cycleResponse.json();
+  } else {
+    throw new Error(`Received ${cycleResponse.status} status from Whoop`);
+  }
 }
 
 export async function getRecovery(token, cycle) {
 
-    const uri = `https://api.prod.whoop.com/developer/v1/cycle/${cycle}/recovery`;
-    
-    const recoveryResponse = await fetch(uri, {
-        method:'GET',
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': 'https://whoop-performance-frontend-a54d4e25e6a0.herokuapp.com',
+  const uri = `https://whoop-performance-backend-e57e252e2747.herokuapp.com/api/recovery/${token}/${cycle}`;
+  
+  const recoveryResponse = await fetch(uri, {
+    method:'GET',
+  });
 
-        },
-    });
-
-    if (recoveryResponse.status === 200) {
-        return recoveryResponse.json();
-    } else {
-        throw new Error(`Received ${recoveryResponse.status} status from Whoop`);
-    }
+  if (recoveryResponse.status === 200) {
+      return recoveryResponse.json();
+  } else {
+      throw new Error(`Received ${recoveryResponse.status} status from Whoop`);
+  }
 }
--- src/Controllers/WorkoutController.js
+++ src/Controllers/WorkoutController.js
@@ -1,22 +1,9 @@
 export async function getLastWorkouts(accessToken){
     try{
-        const query = new URLSearchParams({
-            limit: "10",
-        });
-
-        const uri = `https://api.prod.whoop.com/developer/v1/activity/workout?${query}`
-        const auth = `Bearer ${accessToken}`
-        console.log(auth)
-        console.log(accessToken)
-
+        const uri = `https://whoop-performance-backend-e57e252e2747.herokuapp.com/api/workout/getLast10Workouts/${accessToken}`
         const response = await fetch(uri, {
             method: 'GET',
-            headers: {
-                Authorization: auth,
-                'Content-Type': 'application/json'
-            }
         });
-        
 
         if(response.status === 200){
             const data = await response.json();
@@ -28,40 +15,12 @@ export async function getLastWorkouts(accessToken){
     }
 }
 
-// Function to format date and time as a string
-function formatDateToISO(date) {
-    let year = date.getFullYear();
-    let month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
-    let day = String(date.getDate()).padStart(2, '0');
-    let hours = String(date.getHours()).padStart(2, '0');
-    let minutes = String(date.getMinutes()).padStart(2, '0');
-    let seconds = String(date.getSeconds()).padStart(2, '0');
-
-    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
-}
-
 export async function getWorkoutByDates(accessToken){
-    // Current date and time
-    let currentDate = new Date();
-    // Calculating the date and time for two days ago
-    let twoDaysAgo = new Date();
-    twoDaysAgo.setDate(currentDate.getDate() - 3);
 
-
-    const query = new URLSearchParams({
-        start: formatDateToISO(twoDaysAgo),
-        end: formatDateToISO(currentDate),
-    });
-
-    const uri = `https://api.prod.whoop.com/developer/v1/activity/workout?${query}`
+    const uri = `https://whoop-performance-backend-e57e252e2747.herokuapp.com/api//workout/getWorkoutByDates/${accessToken}`
 
     const response = await fetch(uri, {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'https://whoop-performance-frontend-a54d4e25e6a0.herokuapp.com',
-        }
+        method: 'GET',            
     });
 
     if(response.status === 200){
@@ -71,15 +30,10 @@ export async function getWorkoutByDates(accessToken){
 }
 
 export async function getWorkoutById(accessToken, id){
-    const uri = `https://api.prod.whoop.com/developer/v1/activity/workout/${id}`
+    const uri = `https://whoop-performance-backend-e57e252e2747.herokuapp.com/api/workout/getWorkoutById/${accessToken}/${id}`
 
     const response = await fetch(uri, {
         method: 'GET',
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'https://whoop-performance-frontend-a54d4e25e6a0.herokuapp.com',
-        }
     });
 
     if(response.status === 200){
