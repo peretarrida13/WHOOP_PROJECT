48551b6703184691659ddbb1f50ac8e6d034b7d0
Pere Tarrida Domingo <58071471+peretarrida13@users.noreply.github.com>
2024-01-29 20:18:58 +0100

    change json

--- src/Utils/bodypartParse.js
+++ src/Utils/bodypartParse.js
@@ -27,7 +27,6 @@ export const getMusclesFromWorkouts = (exercises) => {
                         workouts[j].muscles[k].frequency = 0;
                     }
                     
-                    workouts[j].muscles[k].name = workouts[j].sport;
                     if(shouldAdd(workouts[j].muscles[k], muscles)) muscles.push(workouts[j].muscles[k]);
                 }
             }
