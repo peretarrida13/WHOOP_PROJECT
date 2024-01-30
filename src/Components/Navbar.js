a84084dbe80660609093f500b2f23b73d611fc4e
Pere Tarrida Domingo <58071471+peretarrida13@users.noreply.github.com>
2024-01-30 15:52:59 +0100

    main

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,19 +13,21 @@ export default function Navbar() {
 
     return(
         <Box sx={{display:'flex', flexDirection:'row', backgroundColor:'transparent', justifyContent:'center', alignItems:'center'}}>
-            <a href="/">
+            <a href="/dashboard">
                 <img alt="navbar logo" src={require("../Images/projectLogoDark.png")} style={{justifyContent: 'flex-start', marginLeft: 15,  height:'100px'}} />
-           </a>
-           { window.location.pathname === '/login' ? <Box sx={{ flexGrow: 0.7 }} /> :
-            <Box sx={{ flexGrow: 0.8 }} />
-           }
-           <img src={require('../Images/possibleLogoTransparent.png')} alt="Whoop Logo" style={{justifyContent:'flex-end', height:'45px'}}/>
-           <Box sx={{ flexGrow: 1 }} />
-           { window.location.pathname === '/login' ? null :
+            </a>
+            { window.location.pathname === '/login' ? <Box sx={{ flexGrow: 0.7 }} /> :
+                <Box sx={{ flexGrow: 0.8 }} />
+            }
+            <div href="/dashboard">
+                <img src={require('../Images/possibleLogoTransparent.png')} alt="Whoop Logo" style={{justifyContent:'flex-end', height:'45px'}}/>
+            </div>
+            <Box sx={{ flexGrow: 1 }} />
+            { window.location.pathname === '/login' ? null :
                 <Button variant="contained" style={{height:40, marginRight:30, backgroundColor:'#00F09F', fontWeight:700}} onClick={handleLogout}>
                     <p className='headline'>Log Out</p>
                 </Button>
-           }
+            }
         </Box>
     )
 }
